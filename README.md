7f5dbda02b0895b23dbc44f0109dbe2f374fdfa5
Gregoire Pailler <gregoire@pailler.fr>
2016-05-06 17:15:40 +0800

    tPresent initial push on GitHub

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # tPresent
-Presence web application (Sqwiggle like)
+**tPresent** is a presence web application. It replaces [Sqwiggle](https://www.sqwiggle.com/) still pictures feature.
+
+### Usage
+You can use online version hosted on https://tPresent.pythonanywhere.com or run you own instance.
+
+![tpresent](https://cloud.githubusercontent.com/assets/3621529/15069064/d9e2ceb2-13ad-11e6-90c8-faca729c53aa.jpg)
+
+### Limitations
+Tested on Chrome Windows/Mac only
---
+++ flask_app.py
@@ -0,0 +1,84 @@
+from flask import Flask, render_template, request, jsonify, abort
+from werkzeug.contrib.cache import SimpleCache
+from datetime import datetime, timedelta
+
+DEBUG = False
+BIND = '127.0.0.1'
+SSL = False
+
+REFRESH_INTERVAL = 60  # Seconds
+CACHE_RETENTION = REFRESH_INTERVAL * 2
+CAPTURE_WIDTH = 640
+CAPTURE_HEIGHT = 480
+
+
+cache = SimpleCache()
+app = Flask(__name__)
+
+
+class RoomCache:
+    def __init__(self):
+        self.cache = {}
+
+    def add(self, user, image):
+        self.cache[user] = (datetime.utcnow(), image)
+
+    def get(self):
+        self.__clean()
+
+        result = {}
+        for key, value in list(self.cache.items()):
+            result[key] = value[1]
+
+        return result
+
+    def __clean(self):
+        for key, value in list(self.cache.items()):
+            delta = timedelta(seconds=CACHE_RETENTION)
+            if value[0] + delta < datetime.utcnow():
+                del self.cache[key]
+
+
+@app.route('/')
+def default():
+    return render_template('index.html',
+                           refresh_interval=REFRESH_INTERVAL * 1000,
+                           capture_width=CAPTURE_WIDTH,
+                           capture_height=CAPTURE_HEIGHT)
+
+
+@app.route('/presence/<room_name>', methods=['GET'])
+def get_presence(room_name):
+    cache_entry = cache.get(room_name)
+    if cache_entry is None:
+        abort(404)
+    else:
+        return jsonify({'room_name': room_name, 'members': cache_entry.get()})
+
+
+@app.route('/presence/<room_name>', methods=['POST'])
+def presence(room_name):
+    cache_entry = cache.get(room_name)
+
+    args = request.get_json()
+    if args is None:
+        abort(500)
+
+    display_name = args.get('display_name')
+    image = args.get('image', None)
+    if len(display_name) == 0 or len(image) == 0:
+        abort(500)
+
+    if cache_entry is None:
+        cache_entry = RoomCache()
+
+    cache_entry.add(display_name, image)
+    cache.set(room_name, cache_entry, timeout=CACHE_RETENTION)
+
+    return ''
+
+
+if __name__ == '__main__':
+    app.run(debug=DEBUG,
+            host=BIND,
+            ssl_context='adhoc' if SSL else None)
